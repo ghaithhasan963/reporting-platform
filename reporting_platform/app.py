7fcdc4e9c9b389be42b046d9d60f8f5e8dab9fdf
ghaith hasan <ghaithhasan963@gmail.com>
2025-07-29 23:18:26 +0300

    Update app.py

--- reporting_platform/app.py
+++ reporting_platform/app.py
@@ -100,5 +100,6 @@ def logout():
     return redirect('/login')
 
 if __name__ == '__main__':
-    db.create_all()
+    with app.app_context():
+        db.create_all()
     app.run(host='0.0.0.0', port=10000, debug=True)
