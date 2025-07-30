d5a32d6a7108b21691354daa5e8d84820bb3b23b
ghaith hasan <ghaithhasan963@gmail.com>
2025-07-30 12:33:07 +0300

    Create comments.py

---
+++ reporting_platform/comments.py
@@ -0,0 +1,10 @@
+from reporting_platform.app import db, Comment
+from datetime import datetime
+
+def add_comment(report_id, username, text):
+    c = Comment(report_id=report_id, username=username, text=text, timestamp=datetime.utcnow())
+    db.session.add(c)
+    db.session.commit()
+
+def get_comments(report_id):
+    return Comment.query.filter_by(report_id=report_id).order_by(Comment.timestamp.desc()).all()
