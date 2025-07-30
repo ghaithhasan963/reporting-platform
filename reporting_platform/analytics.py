feaf354979b417d05d225f2393357cb8a4a9c240
ghaith hasan <ghaithhasan963@gmail.com>
2025-07-30 12:32:21 +0300

    Create analytics.py

---
+++ reporting_platform/analytics.py
@@ -0,0 +1,21 @@
+from reporting_platform.app import Report
+from sqlalchemy import func
+from reporting_platform import db
+
+def get_reports_per_day():
+    results = db.session.query(
+        func.date(Report.created_at),
+        func.count(Report.id)
+    ).group_by(func.date(Report.created_at)).all()
+    return results
+
+def get_reports_by_location():
+    results = db.session.query(
+        func.round(Report.latitude, 2),
+        func.round(Report.longitude, 2),
+        func.count(Report.id)
+    ).group_by(
+        func.round(Report.latitude, 2),
+        func.round(Report.longitude, 2)
+    ).all()
+    return results
